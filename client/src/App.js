1dadabb31adfa9558d0c6b3b8cf5b1ad484cc64a
Thandriss <mahonko_yana@mail.ru>
2024-01-22 11:47:45 +0200

    not found page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Data from './component/Data'
 import Main from './component/Main';
+import NotFound from './component/NotFound';
 
 function App() {
   
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Main/>}></Route>
         <Route path='/book/:id' element={<Data/>}></Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
     
